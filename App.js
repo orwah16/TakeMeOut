8973010a09e3ca2ab14b311afba0cc94cab9ecf4
orwah16 <orwah_wattad@hotmail.com>
2021-04-03 19:28:02 +0300

    saving data using redux and firestore

--- App.js
+++ App.js
@@ -3,6 +3,11 @@ import React,{Component} from 'react';
 import { Text, View } from 'react-native';
 
 import * as firebase from 'firebase'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/reducers'
+import thunk from 'redux-thunk'
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -24,6 +29,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
 import LoginScreen from './components/auth/Login'
+import MainScreen from './components/Main'
 
 
 const Stack = createStackNavigator();
@@ -32,7 +38,7 @@ export class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      loaded:true,//change it later to reflect work
+      loaded:false,
     }
   }
   componentDidMount(){
@@ -66,14 +72,15 @@ export class App extends Component {
             <Stack.Screen name="Landing" component={LandingScreen} options={{headerShown:false}}></Stack.Screen>
             <Stack.Screen name="Register" component={RegisterScreen}></Stack.Screen>
             <Stack.Screen name="Login" component={LoginScreen}></Stack.Screen>
+            <Stack.Screen name="Main" component={MainScreen}></Stack.Screen>
           </Stack.Navigator>
         </NavigationContainer>
       );
     }
     return(
-      <View style={{flex: 1,justifyContent: 'center'}}>
-      <Text>User is logged in</Text>
-      </View>
+      <Provider store={store}>
+        <MainScreen/>
+      </Provider>
     )
   }
 }
---
+++ components/Main.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native';
+import { connect } from 'react-redux'
+import {bindActionCreators} from 'redux'
+import { fetchUser } from '../redux/actions/index'
+export class Main extends Component {
+    componentDidMount(){
+        this.props.fetchUser();
+    }
+    render() {
+        const { currentUser } = this.props;
+        console.log(currentUser)
+        return (
+        <View style={{flex: 1,justifyContent: 'center'}}>
+            <Text>User is logged in</Text>
+        </View>
+        )
+    }
+}
+
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser
+})
+const mapDispatchProps = (dispatch) => bindActionCreators({fetchUser},dispatch);
+export default connect(mapStateToProps,mapDispatchProps)(Main);
