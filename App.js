df7e60dee6109826f5f7ee6ce230f5737b2f7155
orwah16 <orwah_wattad@hotmail.com>
2021-05-22 01:30:50 +0300

    image picker and saving images .. not complete

--- App.js
+++ App.js
@@ -30,7 +30,8 @@ import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
 import LoginScreen from './components/auth/Login'
 import MainScreen from './components/Main'
-
+import AddScreen from './components/main/Add'
+import SaveScreen from './components/main/Save'
 
 const Stack = createStackNavigator();
 
@@ -81,6 +82,8 @@ export class App extends Component {
         <NavigationContainer>
           <Stack.Navigator initialRoutName="Main">
             <Stack.Screen name="Main" component={MainScreen} options={{headerShown:false}}></Stack.Screen>
+            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation}></Stack.Screen>
+            <Stack.Screen name="Save" component={SaveScreen}></Stack.Screen>
           </Stack.Navigator>  
         </NavigationContainer>
       </Provider>
