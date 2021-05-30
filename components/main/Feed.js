31ba240d6b53744964ca2bb21e5ad9c0b7bd34b3
orwah16 <orwah_wattad@hotmail.com>
2021-05-30 18:34:42 +0300

    added vertical list to feed for search results

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -3,16 +3,22 @@ import {View,StyleSheet,StatusBar} from 'react-native'
 import SearchBar  from './SearchBar'
 import SearchCard from './SearchCard'
 import ScreenTop from './ScreenTop'
-
+import data from './fakeData'
+import VerticalList from './VerticalList'
 export default function Feed() {
+    //const tag = data.filter(item = > item.categroy === 'tag') for filtering
     return (
-        <View>
-            <ScreenTop>
-                <SearchBar/>
-            </ScreenTop>
-            <SearchCard/>
-        </View>
+        <React.Fragment>
+            <SearchBar/>
+            
+            <VerticalList data={data}/>
+
+        </React.Fragment>
     )
 }
+
+{/* <ScreenTop>
+
+</ScreenTop> */}
 
 const styles = StyleSheet.create({})
